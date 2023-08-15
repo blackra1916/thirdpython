8761db425d071ba9d807bee07464463c2a16a9b0
blackra1916 <141958845+blackra1916@users.noreply.github.com>
2023-08-15 18:28:59 -0400

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Datascience Tools and Ecosystem
+
+# # Objectives
+
+# In this notebook, Data Science Tools and Ecosystem are summarized..... 
+
+# # Objectives
+# objectives = ['Create a Jupyter Notebook', 'Insert code and markdown cells', 'Share your notebooks through GitHub']
+# for objective in objectives:
+#     print("*",objective)
+
+# In[18]:
+
+
+objectives = ['Create a Jupyter Notebook', 'Insert code and markdown cells', 'Share your notebooks through GitHub'] 
+for objective in objectives:
+    print("*", objective)
+
+
+# Some of the popular languages data scientists use are:
+# Python
+# R
+# Juliya
+# 
+# p=ordered(Python,r,Juliya)
+# print(p)
+# 
+
+# 
+
+# In[5]:
+
+
+orderedlist = ['Python','R','Julia']
+for index, value in enumerate(orderedlist, 1):
+    print("{}. {}".format(index, value))
+
+
+# In[1]:
+
+
+orderedlist = ["Python", "R", "Julia"]
+print(orderedlist)
+
+
+# Some of the commonly used libraries used by data scientists include:
+# 
+
+# In[6]:
+
+
+libraries = ['Pandas', 'Numpy', 'ggplot']
+for index, value in enumerate(libraries, 1):
+    print("{}. {}".format(index, value))
+
+
+# |Data Science Tools|
+# |------------------|
+# |GitHub|
+# |Jupyter Notebooks|
+# |R Studio|
+
+# ### Below are a few examples of evaluating arithmatic expressions in Python
+# 
+
+# This is a simple arthmetic expression to multiply then add integers
+# 
+
+# In[7]:
+
+
+(3*4)+5
+
+
+# This will convert 200 minutes to hours by dividing by 60
+
+# In[8]:
+
+
+200/60
+
+
+# ## Author
+# Rick Black
+
+# In[12]:
+
+
+itemized_list = ['item 1',
+                 ['item 1, subitem 1'],
+                 'item 2',
+                 'item 3',
+                 ['item 3, subitem 1',
+                  'item 3, subitem 2']]
+add_list(document, itemized_list)
+
+
+# In[ ]:
+
+
+
+
